a44460eda00c53cf9851cafab72657afe8ef7285
andrepolischuk <andre.polischuk@gmail.com>
2015-10-11 16:37:19 +0200

    Use `exec` instead of `execFile`

--- test.js
+++ test.js
@@ -1,10 +1,11 @@
 import test from 'ava';
-import execFile from 'exec-file';
+import {exec} from 'child_process';
+const babel = __dirname + '/node_modules/.bin/babel-node';
 
 test('should return last rates', t => {
   t.plan(2);
 
-  execFile('cli.js', {cwd: __dirname}, (err, stdout) => {
+  exec(`${babel} cli.js`, {cwd: __dirname}, (err, stdout) => {
     t.ifError(err);
     t.true(stdout.trim().indexOf('USD') > 0);
   });
@@ -13,7 +14,7 @@ test('should return last rates', t => {
 test('should return rates for specified date', t => {
   t.plan(2);
 
-  execFile('cli.js', ['2015.5.15'], {cwd: __dirname}, (err, stdout) => {
+  exec(`${babel} cli.js 2015.5.15`, {cwd: __dirname}, (err, stdout) => {
     t.ifError(err);
     t.true(stdout.trim().indexOf('USD') > 0);
   });
