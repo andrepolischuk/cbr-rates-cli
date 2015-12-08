a75994bf7c1b1e1326b9cb84814f10098f74541f
andrepolischuk <andre.polischuk@gmail.com>
2015-12-08 14:39:11 +0300

    Bump ava and babel

--- test.js
+++ test.js
@@ -1,21 +1,12 @@
 import test from 'ava';
-import {exec} from 'child_process';
-const babel = __dirname + '/node_modules/.bin/babel-node';
+import execa from 'execa';
 
-test('should return last rates', t => {
-  t.plan(2);
-
-  exec(`${babel} cli.js`, {cwd: __dirname}, (err, stdout) => {
-    t.ifError(err);
-    t.true(stdout.trim().indexOf('USD') > 0);
-  });
+test('should return last rates', async t => {
+  const {stdout} = await execa('./cli.js');
+  t.true(stdout.trim().indexOf('USD') > 0);
 });
 
-test('should return rates for specified date', t => {
-  t.plan(2);
-
-  exec(`${babel} cli.js 2015.5.15`, {cwd: __dirname}, (err, stdout) => {
-    t.ifError(err);
-    t.true(stdout.trim().indexOf('USD') > 0);
-  });
+test('should return rates for specified date', async t => {
+  const {stdout} = await execa('./cli.js', ['2015.5.15']);
+  t.true(stdout.trim().indexOf('USD') > 0);
 });
